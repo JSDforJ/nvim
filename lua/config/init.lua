6d68c8edfeae854dcc312cb173095cadd7a72bf1
Hjalte Bast <mail@jaysi.cc>
2026-08-12 18:00:53 +0200

    add various plugins

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -2,6 +2,8 @@
 vim.keymap.set("n", "<Space>", "<Nop>", { silent = true })
 vim.g.mapleader = " "
 
+vim.cmd([[colorscheme tokyonight-moon]])
+
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
@@ -26,67 +28,70 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" }
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Diagnostics to loclist" })
 
-local builtin = require "telescope.builtin"
+local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 
-require("nvim-treesitter").install { "rust", "lua", "nix" }
+require("nvim-treesitter").install({ "rust", "lua", "nix" })
 
-require('mason-tool-installer').setup {
-  ensure_installed = {
-    'stylua',
-    'nixfmt',
-    'luacheck',
-    'lua-language-server',
-    'harper-ls',
-    'rust-analyzer',
-    'nil',
-  },
-  auto_update = true,
-}
+require("mason-tool-installer").setup({
+	ensure_installed = {
+		"stylua",
+		"nixfmt",
+		"luacheck",
+		"lua-language-server",
+		"harper-ls",
+		"rust-analyzer",
+		"nil",
+	},
+	auto_update = true,
+})
+
+vim.opt.laststatus = 3
+vim.opt.splitkeep = "screen"
 
 -- You dont need to set any of these options. These are the default ones. Only
 -- the loading is important
-require("telescope").setup {
-  extensions = {
-    fzf = {
-      fuzzy = true, -- false will only do exact matching
-      override_generic_sorter = true, -- override the generic sorter
-      override_file_sorter = true, -- override the file sorter
-      case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-      -- the default case_mode is "smart_case"
-    },
-  },
-}
+require("telescope").setup({
+	extensions = {
+		fzf = {
+			fuzzy = true, -- false will only do exact matching
+			override_generic_sorter = true, -- override the generic sorter
+			override_file_sorter = true, -- override the file sorter
+			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+			-- the default case_mode is "smart_case"
+		},
+	},
+})
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
-require("telescope").load_extension "fzf"
+require("telescope").load_extension("fzf")
 
-require("conform").setup {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    rust = { "rustfmt" },
-    nix = { "nixfmt" },
-    -- Conform will run multiple formatters sequentially
-    -- You can customize some of the format options for the filetype (:help conform.format)
-    -- Conform will run the first available formatter
-  },
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_format = "fallback",
-  },
-}
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		rust = { "rustfmt" },
+		nix = { "nixfmt" },
+		-- Conform will run multiple formatters sequentially
+		-- You can customize some of the format options for the filetype (:help conform.format)
+		-- Conform will run the first available formatter
+	},
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_format = "fallback",
+	},
+})
 
 require("lint").linters_by_ft = {
-  lua = { "luacheck" },
-  rust = { "clippy" },
+	lua = { "luacheck" },
+	rust = { "clippy" },
 }
 
 vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
-  callback = function()
-    require("lint").try_lint()
-  end,
+	callback = function()
+		require("lint").try_lint()
+	end,
 })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,40 +1,152 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  "nvim-tree/nvim-web-devicons",
-  {
-    "nvim-treesitter/nvim-treesitter",
-    lazy = false,
-    build = ":TSUpdate",
-  },
-  "neovim/nvim-lspconfig",
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {},
-    dependencies = {
-      { "mason-org/mason.nvim", opts = {} },
-      "neovim/nvim-lspconfig",
-    },
-  },
-  "nvim-lua/plenary.nvim",
-  {
-    "nvim-telescope/telescope.nvim",
-    version = "*",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      -- optional but recommended
-      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    },
-  },
-  {
-    "stevearc/conform.nvim",
-    opts = {},
-  },
-  'WhoIsSethDaniel/mason-tool-installer.nvim',
-  "mfussenegger/nvim-lint",
-  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-  {
-    "mason-org/mason.nvim",
-    opts = {},
-  },
-  "nvim-treesitter/nvim-treesitter-context",
+	"nvim-tree/nvim-tree.lua",
+	"nvim-tree/nvim-web-devicons",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		build = ":TSUpdate",
+	},
+	"neovim/nvim-lspconfig",
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {},
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+	},
+	"nvim-lua/plenary.nvim",
+	{
+		"nvim-telescope/telescope.nvim",
+		version = "*",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			-- optional but recommended
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		},
+	},
+	{
+		"nvim-mini/mini.icons",
+		version = "*",
+		config = function()
+			require("mini.icons").setup()
+		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+	},
+	-- lazy.nvim
+	{
+		"folke/snacks.nvim",
+		lazy = false,
+		priority = 1000,
+		---@type snacks.Config
+		opts = {
+			indent = {
+				-- your indent configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			},
+			image = {},
+			animate = {},
+			terminal = {},
+		},
+	},
+	{
+		"folke/edgy.nvim",
+		---@module 'edgy'
+		---@param opts Edgy.Config
+		opts = function(_, opts)
+			for _, pos in ipairs({ "top", "bottom", "left", "right" }) do
+				opts[pos] = opts[pos] or {}
+				table.insert(opts[pos], {
+					ft = "snacks_terminal",
+					size = { height = 0.4 },
+					title = "%{b:snacks_terminal.id}: %{b:term_title}",
+					filter = function(_buf, win)
+						return vim.w[win].snacks_win
+							and vim.w[win].snacks_win.position == pos
+							and vim.w[win].snacks_win.relative == "editor"
+							and not vim.w[win].trouble_preview
+					end,
+				})
+			end
+		end,
+	},
+	{
+		"tanvirtin/vgit.nvim",
+		dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
+		-- Lazy loading on 'VimEnter' event is necessary.
+		event = "VimEnter",
+		config = function()
+			require("vgit").setup()
+		end,
+	},
+	"WhoIsSethDaniel/mason-tool-installer.nvim",
+	"mfussenegger/nvim-lint",
+	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	{
+		"mason-org/mason.nvim",
+		opts = {},
+	},
+	{
+		"Bekaboo/dropbar.nvim",
+		-- optional, but required for fuzzy finder support
+		dependencies = {
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+		},
+		config = function()
+			local dropbar_api = require("dropbar.api")
+			vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
+			vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
+			vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
+		end,
+	},
+	{
+		"folke/trouble.nvim",
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
+	},
+	"nvim-treesitter/nvim-treesitter-context",
 }
